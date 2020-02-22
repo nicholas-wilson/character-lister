1a2c40683ad5a9b255482ffbc115d9cb0265e609
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-22 03:56:00 +0000

    added some basic routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,37 @@ class ApplicationController < Sinatra::Base
     erb :layout
   end
 
+  get '/signup' do
+    :signup
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:password] != ""
+      if params[:username].include?(" ") && params[:password].include?(" ")
+        redirect '/signup'
+      end
+      # then create the user
+      redirect '/index'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    if params[:username] != "" && params[:password] != ""
+      user = User.find_by(username: params[:username])
+      if !!user && user.validate(params[:password])
+        session[:user_id] = user.id
+        redirect '/index'
+      else
+        redirect '/login'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
