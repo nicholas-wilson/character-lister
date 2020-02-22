7bb0dc93f49239493aaf568e4e6e75fa56881484
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-22 01:20:19 +0000

    added tests for characters

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,6 +1,23 @@
 require 'pry'
 describe 'Character' do
   before do
-    @character = Character.create(:name => "Ash Ketchup", :description => "") #writing tests for character still
+    @user = User.create(:username => "nictheguy", :password => "dog")
+    @character = Character.create(:name => "Ash Ketchup", :description => "Throws tomato based sauce on foes.", :liked_because => "he's neat.", :list_rank => 1, :user_id => 1) #writing tests for character still
+  end
+
+  it "has a decription" do
+    expect(@character.description).to eq("Throws tomato based sauce on foes.")
+  end
+
+  it "has a reason they're liked" do
+    expect(@character.liked_because).to eq("he's neat.")
+  end
+
+  it "has a list_rank" do
+    expect(@character.list_rank).to eq(1)
+  end
+
+  it "has a user" do
+    expect(@character.user).to be_an_instance_of(User)
   end
 end
