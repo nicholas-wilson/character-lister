3e61d2cdb0629a0f66b5a9d9244813585bd67e52
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-22 07:03:28 +0000

    Added forms for Signin and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
 
+  get '/home' do
+    if !Helper.logged_in?(session)
+      redirect '/login'
+    end
+    @user = Helper.current_user(session)
+    erb :"users/index"
+  end
 
+  get '/signup' do
+    if Helper.logged_in?(session)
+      redirect '/home'
+    end
+    erb :"users/signup"
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:password] != ""
+      if params[:username].include?(" ") && params[:password].include?(" ")
+        redirect '/signup'
+      end
+      new_person = User.create(params)
+      session[:user_id] = new_person.id
+      redirect '/home'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if Helper.logged_in?(session)
+      redirect '/home'
+    end
+    erb :"users/login"
+  end
+
+  post '/login' do
+    if params[:username] != "" && params[:password] != ""
+      user = User.find_by(username: params[:username])
+      if !!user && user.validate(params[:password])
+        session[:user_id] = user.id
+        redirect '/index'
+      else
+        redirect '/login'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
